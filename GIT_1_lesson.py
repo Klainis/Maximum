34b98485d6aa07accc10cbdf8ff043d5d62d62a2
Klainis <136218846+Klainis@users.noreply.github.com>
2023-06-21 16:18:53 +0500

    Update GIT_1_lesson.py

--- GIT_1_lesson.py
+++ GIT_1_lesson.py
@@ -2,19 +2,9 @@
 
 def palindrom(s):
     
-    if len(s)%2==0:
-        s1 = s[:len(s)//2]
-        s2 = s[len(s)//2:] 
-        if s1 == s2[::-1]:
-            return True
-        else:
-            return False
-    elif  len(s)%2!=0:
-        s1 = s[:(len(s)-1)//2]
-        s2 = s[(len(s)-1)//2+1:]
-        if s1 == s2[::-1]:
-            return True 
-        else:
-            return False
+    if s == s[::-1]:
+        return True
+    else:
+        return False
 
-print(palindrom('tenet'))
+print(palindrom('lepsspel'))
